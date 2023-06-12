fc699df0325fbadf6e699ffc0dcac99032834806
abishekk92 <bhat.abishek@gmail.com>
2023-06-12 19:03:17 +0530

    Add and integrate gpl_session

--- client/src/contexts/initialize_game.rs
+++ client/src/contexts/initialize_game.rs
@@ -1,9 +1,11 @@
 use crate::*;
+use solana_sdk::sysvar;
 
 pub fn initialize_game(
     client: &Client,
     user: Pubkey,
     game: Pubkey,
+    is_rated: bool,
     wager: Option<u64>,
 ) -> ClientResult<()> {
     let initiallize_game_ix = Instruction {
@@ -13,8 +15,9 @@ pub fn initialize_game(
             AccountMeta::new(user, false),
             AccountMeta::new(game, false),
             AccountMeta::new_readonly(system_program::ID, false),
+            AccountMeta::new_readonly(sysvar::clock::id(), false),
         ],
-        data: sol_chess::instruction::InitializeGame { wager }.data(),
+        data: sol_chess::instruction::InitializeGame { wager, is_rated }.data(),
     };
 
     send_and_confirm_tx(
--- programs/sol-chess/src/contexts/move_piece.rs
+++ programs/sol-chess/src/contexts/move_piece.rs
@@ -1,26 +1,74 @@
+use gpl_session::SessionError;
+use gpl_session::SessionToken;
+
 use crate::*;
+use anchor_lang::prelude::Result;
 
 #[derive(Accounts)]
 pub struct MovePiece<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
 
-    #[account(mut,address=User::pda(payer.key()).0)]
+    #[account(mut)]
     pub user: Account<'info, User>,
+
     #[account(mut)]
     pub adversary_user: Account<'info, User>,
 
     #[account(mut)]
     pub game: Account<'info, Game>,
+
+    pub session_token: Option<Account<'info, SessionToken>>,
 }
+
 impl<'info> MovePiece<'info> {
     pub fn process(&mut self, from: Square, to: Square) -> Result<()> {
         let Self {
             user,
             game,
             adversary_user,
+            session_token,
+            payer,
             ..
         } = self;
+
+        // BEGIN SessionToken Checks
+
+        if let Some(_) = session_token {
+            let target_program = crate::id();
+            let local_session_token = session_token.clone().unwrap();
+            let local_payer = payer.clone().key;
+
+            require!(
+                user.key() == User::pda(local_session_token.authority).0,
+                SessionError::InvalidToken
+            );
+
+            let seeds = &[
+                SessionToken::SEED_PREFIX.as_bytes(),
+                target_program.as_ref(),
+                local_payer.as_ref(),
+                local_session_token.authority.as_ref(),
+            ];
+
+            let (pda, _) = Pubkey::find_program_address(seeds, &gpl_session::id());
+
+            require_eq!(pda, local_session_token.key(), SessionError::InvalidToken);
+
+            let now = Clock::get()?.unix_timestamp;
+            require!(
+                now < local_session_token.valid_until,
+                SessionError::InvalidToken
+            );
+        } else {
+            require!(
+                user.key() == User::pda(self.payer.key()).0,
+                CustomError::InvalidUser
+            );
+        }
+
+        // END SessionToken Checks
+
         let color = game.get_current_player_color();
 
         require!(
